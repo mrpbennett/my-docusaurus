bbdd4d01fb43a90c9e179b5f34c252e4070287cf
Paul Bennett <pbennett.uk@outlook.com>
2026-07-30 16:25:14 +0100

    Move blog routeBasePath from / to /blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,7 @@ const config = {
       ({
         docs: false,
         blog: {
-          routeBasePath: "/",
+          routeBasePath: "/blog",
           blogTitle: "ramblings from mrpbennett",
           blogDescription: "lalalalalala",
           showReadingTime: true,
@@ -88,10 +88,11 @@ const config = {
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
+          href: "/blog",
         },
         items: [
-          { to: "/", label: "Posts", position: "left" },
-          { to: "/tags", label: "Tags", position: "left" },
+          { to: "/blog", label: "Posts", position: "left" },
+          { to: "/blog/tags", label: "Tags", position: "left" },
           { to: "/about", label: "About", position: "left" },
           {
             href: "https://github.com/mrpbennett",
@@ -111,11 +112,11 @@ const config = {
             items: [
               {
                 label: "All Posts",
-                to: "/",
+                to: "/blog",
               },
               {
                 label: "Tags",
-                to: "/tags",
+                to: "/blog/tags",
               },
             ],
           },
